4f40fe0f7d5201640e468a176a82d6c62eec46c8
James <james@Zipcoders-MacBook-Pro-101.local>
2023-03-10 11:54:03 -0500

    added board

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -1,10 +1,18 @@
 package rocks.zipcodewilmington.tictactoe;
 
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Arrays;
+
 /**
  * @author leon on 6/22/18.
  */
 public class Board {
-    public Board(Character[][] matrix) {
+
+    public Character[][] board;
+    public Board(Character[][] board) {
+        this.board = board;
+        System.out.println(Arrays.deepToString(this.board));
     }
 
     public Boolean isInFavorOfX() {
